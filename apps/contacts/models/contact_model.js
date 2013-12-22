ab10c6cb720225b8ca7351cc5e0c2ce3b78adc11
Matthew Thebert <matt@thebert.com>
2013-12-21 23:40:51 -0500

    Create model for group and contact

---
+++ apps/contacts/models/contact_model.js
@@ -0,0 +1,19 @@
+// ==========================================================================
+// Project:   Contacts.Contact
+// Copyright: @2013 My Company, Inc.
+// ==========================================================================
+/*globals Contacts */
+
+/** @class
+
+  (Document your Model here)
+
+  @extends SC.Record
+  @version 0.1
+*/
+Contacts.Contact = SC.Record.extend(
+/** @scope Contacts.Contact.prototype */ {
+
+  // TODO: Add your own code here.
+
+});
---
+++ apps/contacts/models/group_model.js
@@ -0,0 +1,19 @@
+// ==========================================================================
+// Project:   Contacts.Group
+// Copyright: @2013 My Company, Inc.
+// ==========================================================================
+/*globals Contacts */
+
+/** @class
+
+  (Document your Model here)
+
+  @extends SC.Record
+  @version 0.1
+*/
+Contacts.Group = SC.Record.extend(
+/** @scope Contacts.Group.prototype */ {
+
+  // TODO: Add your own code here.
+
+});
